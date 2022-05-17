bd7c57aea5a5d6a765512382a2d4518ffabf33d2
przemyslaw.matraj <przemyslaw.matraj@microsoft.wsei.edu.pl>
2022-05-17 10:34:48 +0200

    Lab10

---
+++ Lab10/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Lab10
+{
+    class Node<T>
+    {
+        public T Value { get; set; }
+        public Node<T> Left { get; set; }
+        public Node<T> Right { get; set; }
+
+    }
+    class BinaryTree<T>
+    {
+        public Node<T> Root { get; init; }
+        public void PreorderTraversal(Action<T> action)
+        {
+
+        }
+        private void Preorder(Node<T> node, Action<Node<T>> action)
+        {
+            if(node == null)
+            {
+                return;
+            }
+            action.Invoke(node);
+            Preorder(node.Left, action);
+            Preorder(node.Right, action);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Node<string> node = new Node<string>() { Value = "aaa" };
+            node.Left = new Node<string>() { Value = "BBB" };
+            node.Right = new Node<string>() { Value = "CCC"};
+            
+        }
+    }
+}
