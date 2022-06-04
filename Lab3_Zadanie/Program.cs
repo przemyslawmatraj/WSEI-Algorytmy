cb1ace35e40deb1192d0d9edf31e68ec14c61d0f
przemyslawmatraj <Przemyslaw.Matraj@microsoft.wsei.edu.pl>
2022-06-04 22:09:38 +0200

    Lab3 Exercise

---
+++ Lab3_Zadanie/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Lab3_Exercise
+{
+    public class Program
+    {
+        static int fibonnaci(int n)
+        {
+            if (n == 0)
+            {
+                return 0;
+            }
+            if (n == 1)
+            {
+                return 1;
+            }
+            return fibonnaci(n - 1) + fibonnaci(n - 2);
+        }
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine(fibonnaci(n));
+        }
+    }
+}
